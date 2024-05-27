4c1a75f597c3d8bf7a8adfa3ac9322eab7b8b243
AliHamza0007 <141808379+AliHamza0007@users.noreply.github.com>
2024-05-27 17:01:24 +0500

    bf

--- dist/controllers/ProductController.js
+++ dist/controllers/ProductController.js
@@ -24,7 +24,7 @@ export const getAllProducts = TryCatch(async (req, res, next) => {
         baseQuery.category = category;
     const productsPromise = Product.find(baseQuery)
         .sort(sort && { price: sort === 'asc' ? 1 : -1 })
-        .limit(limit).sort({ createdAt: -1 })
+        .limit(limit)
         .skip(skip);
     const [products, filteredOnlyProduct] = await Promise.all([
         productsPromise,
@@ -138,7 +138,7 @@ export const getAdminProduct = TryCatch(async (req, res, next) => {
     if (myCache.has('all-products'))
         products = JSON.parse(myCache.get('all-products'));
     else {
-        products = await Product.find({}).sort({ createdAt: -1 });
+        products = await Product.find({});
         myCache.set('all-products', JSON.stringify(products));
     }
     products.length !== 0
